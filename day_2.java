3f1c60ecc81538882a10289121230d1c0a816324
Jaffar Abbas <gamaportal8@gmail.com>
2020-05-08 00:30:42 +0500

    7-4-2020

---
+++ day_2.java
@@ -0,0 +1,39 @@
+//question s here 
+//https://www.hackerrank.com/challenges/30-operators/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class day_2{
+
+    // Complete the solve function below.
+    static void solve(double meal_cost, int tip_percent, int tax_percent) {
+double a =meal_cost*tip_percent/100;
+double b =meal_cost*tax_percent/100;
+double c = meal_cost+a+b;
+System.out.println(Math.round(c));
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double meal_cost = scanner.nextDouble();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tip_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tax_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        solve(meal_cost, tip_percent, tax_percent);
+
+        scanner.close();
+    }
+}
