4995afa9727ee652e19c799ea19969fe3d4857e6
Jaffar Abbas <gamaportal8@gmail.com>
2020-05-08 15:27:11 +0500

    8-5-2020

---
+++ day_3.java
@@ -0,0 +1,36 @@
+//question s here 
+//https://www.hackerrank.com/challenges/30-conditional-statements/problem
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if(N%2==0 && N>2 && N < 5){
+            System.out.println("Not Weird");
+        }
+        else if(N%2==0 && N>6 && N < 20){
+            System.out.println("Weird");
+        }
+        else if(N%2==0 &&  N > 20){
+            System.out.println("Not Weird");
+        }
+        else{
+            System.out.println("Weird");
+        }
+        scanner.close();
+    }
+}
